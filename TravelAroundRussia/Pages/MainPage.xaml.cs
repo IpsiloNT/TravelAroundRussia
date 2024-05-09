21afa59da03604148db03564105af009c3b8aaa9
Lobanov Kirill <ipsilont954@gmail.com>
2024-05-09 18:10:31 +0300

    Добавьте файлы проекта.

---
+++ TravelAroundRussia/Pages/MainPage.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TravelAroundRussia.Entities;
+
+namespace TravelAroundRussia.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            LV_Tours.ItemsSource = DataBase.entities.Tours.OrderBy(tours => tours.NameTour).ToList();
+
+            List<Entities.Type> types = new List<Entities.Type>();
+            types.Add(new Entities.Type(){ NameType = "Все типы" });
+            types.AddRange(DataBase.entities.Types.OrderBy(type => type.NameType));
+
+            CB_Filter.ItemsSource = types;
+        }
+
+        private void CHB_Is_Actual_Checked(object sender, RoutedEventArgs e)
+        {
+            LV_Tours.ItemsSource = DataBase.entities.Tours.OrderBy(t => t.NameTour).Where(t => t.IsActual == true).ToList();
+        }
+
+        private void CHB_Is_Actual_Unchecked(object sender, RoutedEventArgs e)
+        {
+            LV_Tours.ItemsSource = DataBase.entities.Tours.OrderBy(t => t.NameTour).ToList();
+        }
+
+        private void CB_Filter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (CB_Filter.SelectedItem != null || ((Entities.Type)CB_Filter.SelectedItem).IDType == 0)
+            {
+                LV_Tours.ItemsSource = DataBase.entities.Tours.OrderBy(t => t.NameTour).ToList();
+                return;
+            }
+            else
+            {
+                int selectedTypeId = ((Entities.Type)CB_Filter.SelectedItem).IDType;
+                var filteredType = DataBase.entities.Types.OrderBy(t => t.NameType).Where(t => t.IDType == selectedTypeId).ToList();
+                LV_Tours.ItemsSource = filteredType;
+            }
+        }
+
+        private void TB_Find_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (TB_Find.Text != "")
+            {
+                LV_Tours.ItemsSource = DataBase.entities.Tours.OrderBy(t => t.NameTour).Where(c => c.NameTour.ToLower().Contains(TB_Find.Text)).ToList();
+            }
+            else if (TB_Find.Text == "")
+            {
+                LV_Tours.ItemsSource = DataBase.entities.Tours.OrderBy(t => t.NameTour).ToList();
+                return;
+            }
+        }
+
+        private void RefreshTours()
+        {
+
+        }
+    }
+}
---
+++ TravelAroundRussia/Pages/PAuth.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TravelAroundRussia.Entities;
+
+namespace TravelAroundRussia.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PAuth.xaml
+    /// </summary>
+    public partial class PAuth : Page
+    {
+        public PAuth()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_Log_In_Click(object sender, RoutedEventArgs e)
+        {
+            string login = TB_Login.Text;
+            string password = PB_Password.Password;
+
+            if(string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+                ShowError("Пожалуйста, введите все поля!");
+                return;
+            }
+
+            Client loginClient = DataBase.entities.Clients.FirstOrDefault(x => x.LoginClient == login
+            && x.PasswordClient == password);
+
+            Employee loginEmployee = DataBase.entities.Employees.FirstOrDefault(x => x.LoginEmployee == login
+            && x.PasswordEmployee == password);
+
+            if (loginClient == null && loginEmployee == null)
+            {
+                ShowError("Неверные данные!");
+            }
+            else if (loginClient != null)
+            {
+                NavigationService.Navigate(new MainPage());
+                MessageBox.Show($"Вы вошли как клиент");
+            }
+            else if (loginEmployee != null)
+            {
+                NavigationService.Navigate(new MainPage());
+                MessageBox.Show($"Вы вошли как сотрудник: {loginEmployee.SurnameEmployee} " +
+                    $"{loginEmployee.NameEmployee[0]}.{loginEmployee.MiddleNameEmployee[0]}.");
+            }
+        }
+
+        public void ShowError(string message)
+        {
+            MessageBox.Show(message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
